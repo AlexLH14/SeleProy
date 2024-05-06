441e62803c836c0d72441bf4edf2bd93a5b802ea
alex2 <alex28alex147@gmail.com>
2024-05-06 17:56:29 -0600

    Codigo Main de imagen

---
+++ main.py
@@ -0,0 +1,23 @@
+from imagen.obtenerimg import ImageScraper
+
+def main():
+    while True:
+        print("Seleccione una opción:")
+        print("1. Obtener información del clima")
+        print("2. Descargar Imagenes")
+        choice = input("Ingrese el número de la opción deseada (1 o 2): ")
+
+        if choice == '1':
+            print("hola")
+            break
+        elif choice == '2':
+            # Crea una instancia de ImageScraper con la ruta donde guardarás las imágenes
+            scraper = ImageScraper(r'C:\Users\alex2\Pictures\automatizacion')
+
+            # Llama al método scrape_images con la URL del sitio web de donde deseas obtener las imágenes
+            scraper.scrape_images("https://www.mediotiempo.com/futbol/liga-mx")
+        else:
+            print("Opción inválida. Por favor, seleccione 1 o 2.")
+
+if __name__ == "__main__":
+    main()
